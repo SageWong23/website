c66773e46f2e190d19b4c8d958dabf6444e54098
SageWong23 <sagewong23@gmail.com>
2024-11-04 05:52:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 // Get the popup
 var socialPopup = document.getElementById("socialPopup");
 // Get the button that opens the socialPopup
-var button = document.getElementById("socialButton");
+var socialPopupButton = document.getElementById("socialButton");
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("socialClose")[0];
+var divSocialClose = document.getElementsByClassName("socialClose")[0];
+
 // When the user clicks the button, open the socialPopup
-button.onclick = function() {
+socialPopupButton.onclick = function() {
     socialPopup.style.opacity = "1";
     socialPopup.style.zIndex = "1";
 }
-// When the user clicks on <span> (x), close the socialPopup
-span.onclick = function() {
+// When the user clicks on <div> (x), close the socialPopup
+divSocialClose.onclick = function() {
     socialPopup.style.opacity = "0";
-    socialPopup.style.zIndex = "-1";
+    socialPopup.style.zIndex = "-2";
 }
 // When the user clicks anywhere outside of the socialPopup, close it
-window.onclick = function(event) {
+window.addEventListener("click", function(event) {
     if (event.target == socialPopup) {
         socialPopup.style.opacity = "0";
-        socialPopup.style.zIndex = "-1";
+        socialPopup.style.zIndex = "-2";
     }
+})
+
+var aboutPopup = document.getElementById("aboutPopup");
+var aboutPopupButton = document.getElementById("aboutButton");
+var divAboutClose = document.getElementsByClassName("aboutClose")[0];
+
+aboutPopupButton.onclick = function() {
+    aboutPopup.style.opacity = "1";
+    aboutPopup.style.zIndex = "1";
 }
+divAboutClose.onclick = function() {
+    aboutPopup.style.opacity = "0";
+    aboutPopup.style.zIndex = "-1";
+}
+window.addEventListener("click", function(event) {
+    if (event.target == aboutPopup) {
+        aboutPopup.style.opacity = "0";
+        aboutPopup.style.zIndex = "-2";
+    }
+})
